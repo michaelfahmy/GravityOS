33c782b5de41943c5f248754564f8d6797abc885
M-Sayed <mahmoud_sayed212@hotmail.com>
2015-12-12 12:26:34 +0200

    updating rightClick contextMenu

--- src/sfe/os/Main.java
+++ src/sfe/os/Main.java
@@ -2,13 +2,13 @@ package sfe.os;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -17,7 +17,7 @@ import javafx.util.Pair;
 import java.util.Optional;
 
 public class Main extends Application {
-
+    ContextMenu rightClickMenu4Tiles = new ContextMenu();
     private Stage stage;
     private BorderPane explorer;
     FileSystem fileSystem;
@@ -117,29 +117,13 @@ public class Main extends Application {
     }
 
     private void refresh() {
-        ContextMenu rightClickMenu4Tiles = new ContextMenu();
+
         TilePane tiles = new TilePane();
         tiles.setPrefColumns(8);
         tiles.setHgap(25);
         tiles.setVgap(30);
         tiles.setPadding(new Insets(20));
-
-        tiles.setOnMouseClicked(event -> { // Right click menu for tilePane.
-            if(rightClickMenu4Tiles.getItems().size() > 0) { rightClickMenu4Tiles.getItems().clear(); }
-            MenuItem newFolderItem = new MenuItem("New Folder");
-            MenuItem newFileItem = new MenuItem("New File");
-            MenuItem pasteItem = new MenuItem("Paste");
-            MenuItem propertiesItem = new MenuItem("Properties");
-            newFolderItem.setOnAction(event1 -> newFolderDialog());
-            newFileItem.setOnAction(event1 ->  newFileDialog());
-            pasteItem.setOnAction(event1 -> { fileSystem.paste(); refresh(); });
-            propertiesItem.setOnAction(event1 -> { /* Properties.*/ });
-            SeparatorMenuItem separatorMenuItem1 = new SeparatorMenuItem();
-            SeparatorMenuItem separatorMenuItem2 = new SeparatorMenuItem();
-            rightClickMenu4Tiles.getItems().addAll(newFolderItem, newFileItem, separatorMenuItem1, pasteItem, separatorMenuItem2, propertiesItem);
-            if(event.getButton().equals(MouseButton.SECONDARY)) { rightClickMenu4Tiles.show(explorer, event.getScreenX(), event.getScreenY()); }
-            else { rightClickMenu4Tiles.hide(); }
-        });
+        tiles.setOnMouseClicked(event -> {  TilePaneRightClickContextMenu(event); });
         tiles.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
         populateTiles(tiles);
         explorer.setCenter(tiles);
@@ -180,6 +164,23 @@ public class Main extends Application {
             tiles.getChildren().add(view[i]);
         }
     }
+    private void TilePaneRightClickContextMenu(MouseEvent event) {
+        if(rightClickMenu4Tiles.getItems().size() > 0) { rightClickMenu4Tiles.getItems().clear(); }
+        MenuItem newFolderItem = new MenuItem("New Folder");
+        MenuItem newFileItem = new MenuItem("New File");
+        MenuItem pasteItem = new MenuItem("Paste");
+        MenuItem propertiesItem = new MenuItem("Properties");
+        newFolderItem.setOnAction(event1 -> newFolderDialog());
+        newFileItem.setOnAction(event1 ->  newFileDialog());
+        pasteItem.setOnAction(event1 -> { fileSystem.paste(); refresh(); });
+        propertiesItem.setOnAction(event1 -> { /* Properties.*/ });
+        SeparatorMenuItem separatorMenuItem1 = new SeparatorMenuItem();
+        SeparatorMenuItem separatorMenuItem2 = new SeparatorMenuItem();
+        rightClickMenu4Tiles.getItems().addAll(newFolderItem, newFileItem, separatorMenuItem1, pasteItem, separatorMenuItem2, propertiesItem);
+        if(event.getButton().equals(MouseButton.SECONDARY)) { rightClickMenu4Tiles.show(explorer, event.getScreenX(), event.getScreenY()); }
+        else { rightClickMenu4Tiles.hide(); }
+    }
+
 
     private void setIcon(Directory dir, Label view) {
         if (dir instanceof Folder) {
@@ -213,6 +214,7 @@ public class Main extends Application {
     }
 
     private ContextMenu dirRightClickMenu(Directory dir) {
+
         ContextMenu rightClickMenu = new ContextMenu();
         MenuItem openItem = new MenuItem("Open");
         openItem.setOnAction(e -> {
